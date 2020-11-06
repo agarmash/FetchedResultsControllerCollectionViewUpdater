a0709a55816615843e449d5122aa38fc245b64b9
Artem Garmash <a.garmash.dev@gmail.com>
2020-11-06 14:53:29 +0300

    Remove example

--- Example/Pods/Target Support Files/FetchedResultsControllerCollectionViewUpdater/FetchedResultsControllerCollectionViewUpdater-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FetchedResultsControllerCollectionViewUpdater : NSObject
-@end
-@implementation PodsDummy_FetchedResultsControllerCollectionViewUpdater
-@end
